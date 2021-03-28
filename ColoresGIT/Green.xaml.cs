c52fdea458ceedf9ab4e8334fc37a62c6a7592bc
Osman Gomez <gomezosman05@gmail.com>
2021-03-27 18:22:34 -0600

    Listo, SOY del verde y soy feliz :v

---
+++ ColoresGIT/Green.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ColoresGIT
+{
+    /// <summary>
+    /// Interaction logic for Green.xaml
+    /// </summary>
+    public partial class Green : Window
+    {
+        public Green()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+
+        }
+    }
+}
